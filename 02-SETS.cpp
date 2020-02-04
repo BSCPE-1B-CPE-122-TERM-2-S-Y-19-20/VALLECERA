75c640de3c57216852e1079b55a06268550a87ed
mel <48536671+lemdoublev@users.noreply.github.com>
2020-02-05 01:09:32 +0800

    Add files via upload

--- 02-SETS.cpp
+++ 02-SETS.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
 using namespace std;
-void printcomplement(int set1[], int set2[],int size1,int size2)
+int seta[100],setb[100],set3[100];
+int size1,size2,size3;
+
+void universal()
 {
+	
+}
+void printset(int set[],int size){
+	int i,j,temp=0;
+	for(i=0;i<size;i++){
+		for (j=i+1;j<size;j++){
+			if(set[i]>set[j])
+			{
+				temp=set[i];
+				set[i]=set[j];
+				set[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<size;i++)
+	{
+		cout << set[i] << " ";
+	}
+}
+void printcompliment(int set1[], int set2[],int size1,int size2){
   int i = 0, j = 0; 
   while (i<size1||j<size2) { 
     if(set1[i]<set2[j])
@@ -20,12 +43,12 @@ void printcomplement(int set1[], int set2[],int size1,int size2)
     } 
   }
   while (i<size2)  
-    cout <<set1[i]<<" "; 	
+    cout <<set1[i]<<" "; 
 }
+
 void printunion(int set1[], int set2[], int size1, int size2)
 {
 	int i=0, j=0;
-	cout << "\nSet A Union Set B : ";
 	while (i < size1 && j < size2)
 	{
 		if (set1[i] < set2[j])
@@ -43,95 +66,50 @@ void printunion(int set1[], int set2[], int size1, int size2)
 	while(j < size2)
     	cout << set2[j++] << " ";	
 }
-void printintersection(int set1[], int set2[], int size1, int size2)
+void intersection()
 {
 	int i=0, j=0;
-	cout << "\nSet A Intersection Set B : ";
 	while (i < size1 && j < size2)
 	{
-		if(set1[i] < set2[j])i++;
-		else if(set2[j] < set1[i])j++;
-		else
-		{
-			cout << set2[j] << " ";
-			i++;
+		if(seta[i]==setb[j]){
+			set3[size3]=seta[i];
+			size3++;
 			j++;
 		}
+		i++;
 	}
 }
 int main()
 {
-	int choice,i,j,temp=0,size,k;
-	int merge[100];
-	int set1[100],size1;
-	int set2[100],size2;
-	
+	int i,j,temp=0,size;
+	menu:
 	cout << "Enter size of Set A : ";
 	cin >> size1;
 	cout << "\nEnter Elements : " << endl;
 	for(i=0;i<size1;i++)
 	{
-		cin >> set1[i];
+		cin >> seta[i];
 	}
 	cout << "\nEnter size of Set B : ";
 	cin >> size2;
 	for(i=0;i<size2;i++)
 	{
-		cin >> set2[i];
+		cin >> setb[i];
 	}
-	menu:
-	for (i=0;i<size1;i++) 
-    {
-		for (j=i+1;j<size1;j++)
-        {
-			if (set1[i]>set1[j])
-			{
-				temp=set1[i];
-				set1[i]=set1[j];
-				set1[j]=temp;
-			}
-		}
-	}
-	temp=0;
-	for (i=0;i<size2;i++) 
-    {
-		for (j=i+1;j<size2;j++)
-        {
-			if (set2[i]>set2[j])
-			{
-				temp=set2[i];
-				set2[i]=set2[j];
-				set2[j]=temp;
-			}
-		}
-	}	
 	system("cls");
-	cout << "\nSet A = ";
-	for(i=0;i<size1;i++)
-	{
-		cout << set1[i] << ", ";
-	}
-	cout << "\nSet B = ";
-	for(i=0;i<size2;i++)
-	{
-		cout << set2[i] << ", ";
-	}
-	cout << "\n\n1 - Union\t2 - Intersection\n3 - Complement\t4 - Exit\nEnter Choice : ";
-	cin >> choice;
-	switch(choice){
-		case 1:printunion(set1, set2, size1, size2);
-			break;
-		case 2:printintersection(set1, set2, size1, size2);
-			break;
-		case 3:printcomplement(set1, set2, size1,size2);
-			break;
-		case 4: cout << "\nExiting..." <<endl;
-				exit(0); 
-			break;
-		default:
-			cout << "\nInvalid Choice!";
-			system("pause");
-			system("cls");
-			goto menu;
-	}
+	cout << "Set A : ";
+	printset(seta, size1);
+	cout << "\nSet B : ";
+	printset(setb, size2);
+	cout << "\n\n\t\tSet Operation : " << endl;
+	cout << "\nIntersection of the sets : ";
+	intersection();
+	printset(set3, size3);
+	cout << "\nUnion of the sets : ";
+	printunion(seta, setb, size1, size2);
+	cout << "\nCompliment of set A to set B : ";
+	printcompliment(seta,set3,size1,size3);
+	cout << "\nCompliment of set B to set A : ";
+	printcompliment(setb,set3,size2,size3);
+	system("pause");
 }
